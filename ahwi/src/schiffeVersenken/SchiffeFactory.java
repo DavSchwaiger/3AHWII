bdf4665f9b9dc47a5b6ed2d2cdba7ff0f1ed1603
schwa <schwa@LAPTOP-JCRHTL0N>
2022-11-17 10:15:07 +0100

    Schiffe Factory Ansatz

---
+++ ahwi/src/schiffeVersenken/SchiffeFactory.java
@@ -0,0 +1,15 @@
+package schiffeVersenken;
+
+public class SchiffeFactory
+{
+    public static Schiff create(Playground p, int length, boolean orientation)
+    {
+        //Erzeuge ein neues Schiff mit Position und Orientierung: neu
+        //ruft auf: schiffMoeglich(p, neu)
+        //Erstelle so lange ein neues Schiff bis es positionierbar ist
+    }
+
+    public static boolean schiffMoeglich(Playground p, Schiff ship)
+    {
+    }
+}
